8e8e0944857f1752ed743560d5d457cd70c5434c
x90 <x90@x90s-MacBook-Air.local>
2024-03-12 21:41:09 +0200

    Template part formation
    Template formation like:
        #[template: parts/head.html]

--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use std::io;
+use regex::Regex;
+use std::path::Path;
 
 #[derive(Debug, Clone)]
 pub struct TFile {
@@ -17,31 +19,77 @@ impl TFile {
     }
 
     pub fn to_template(&self, name: String) -> Template {
-        Template::new(self.path, name)
+        Template::new(self.path.clone(), name)
+    }
+
+    pub fn to_config(&self) -> Option<Config> {
+        let mut per_page: u32 = 0;
+
+        let contents = self.read().unwrap_or_else(|_| String::from(""));
+        let lines: Vec<&str> = contents.trim().split('\n').collect();
+
+        for line in lines {
+            if line.chars().nth(0).unwrap_or(';') != ';' {
+                let params: Vec<&str> = line.trim().split('=').collect();
+                if params[0].trim() == "per_page" {
+                    per_page = params[1].parse::<u32>().unwrap_or(0);
+                }
+                return Some(Config::new(self.path.clone(), per_page));
+            }
+        }
+        None
     }
 }
 
-trait Templatable {
-    fn to_tfile(path:String) -> TFile {
-        TFile::new(path)
-    }
+trait Fileable {
+    fn to_tfile(&self) -> TFile;
 }
 
-#[derive(Debug)]
+// Template file
+//
+#[derive(Debug, Clone)]
 pub struct Template {
     pub path: String,
     name: String,
 }
 
+impl Fileable for Template {
+    fn to_tfile(&self) -> TFile {
+        TFile::new(self.path.clone())
+    }
+}
 impl Template {
     pub fn new(path: String, name: String) -> Template {
         Template { path, name }
     }
+
+    fn parts(&self) -> String {
+        let contents = self.to_tfile().read();
+        let reg_parts = Regex::new(r"\#\[template\:\ ?(.*?)\]").unwrap();
+        let cont = contents.unwrap_or_else(|_| String::from(""));
+        let parts = reg_parts.captures_iter(cont.as_str());
+
+        let pt = self.path.clone();
+        let path = Path::new(pt.as_str());
+        for part in parts {
+            if let (Some(loc), Some(pth)) = (part.get(1), path.parent()) {
+                    let partfile = TFile::new(format!("{}/{}", pth.display(), loc.as_str())).read().unwrap_or_else(|_| String::from(""));
+                     return Regex::new(format!(r"\#\[template\:\ ?{}\]", loc.as_str()).as_str())
+                                .unwrap()
+                                .replace_all(cont.as_str(), partfile)
+                                .into_owned();
+            }
+        }
+        String::from("")
+    }
+
+    pub fn contents(&self) -> String {
+        self.parts()
+    }
 }
 
-impl Templatable for Template {}
-
-
+// Post file
+//
 #[derive(Debug, Clone)]
 pub struct Post {
     pub path: String,
@@ -75,7 +123,22 @@ impl Post {
     }
 }
 
-impl Templatable for Post {}
+impl Fileable for Post {
+    fn to_tfile(&self) -> TFile {
+        TFile::new(self.path.clone())
+    }
+}
 
+// Config file
+//
+#[derive(Debug, Clone)]
+pub struct Config {
+    path: String,
+    per_page: u32,
+}
 
-
+impl Config {
+    pub fn new(path: String, per_page: u32) -> Config {
+        Config { path, per_page }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,23 @@ mod file;
 fn main() {
     let arguments: CliNop = CliNop::new(env::args().collect());
 
-    let template_base = arguments.get::<String>("template");
+    let template_base = arguments.get::<String>("template").unwrap();
+
+    // Config
+    let config_file = TFile::new(format!("{}/{}.{}", template_base, "config", "cfg"));
+    let config = config_file.to_config();
+    println!("Config: {:?}", config);
+
+    // Template
+    let template_file: TFile = TFile::new(format!(
+        "{}/{}/{}.{}",
+        template_base, "templates", "index", "html"
+    ));
+    let template = template_file.to_template(String::from("index.html"));
+    println!("Template: {:?}", template);
+
+    let template_content = template.contents();
+    println!("TC: {:?}", template_content);
+
+    // Post
 }
